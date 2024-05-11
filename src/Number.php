ee8fb29fbcdced972b74c5bd0c0c3174c3b34a45
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2024-05-11 17:20:49 +0800

    refactor: improved removal of superfluous precision

--- src/Number.php
+++ src/Number.php
@@ -220,7 +220,7 @@ final readonly class Number implements Stringable
         $baseScale = $this->scale;
 
         $num = bcpow($this->value, $exponent, self::TEMPORARY_SCALE);
-        $num = str_ends_with($num, '0000000000') ? rtrim($num, self::ZERO) : $num;
+        $num = self::removeSuperfluousPrecision($num, $baseScale);
         $resultScale = self::determineScale($num);
 
         $scale = ($resultScale > self::MAX_EXPANSION_SCALE)
@@ -240,7 +240,7 @@ final readonly class Number implements Stringable
         $baseScale = $this->scale;
 
         $num = bcsqrt($this->value, self::TEMPORARY_SCALE);
-        $num = str_ends_with($num, '0000000000') ? rtrim($num, self::ZERO) : $num;
+        $num = self::removeSuperfluousPrecision($num, $baseScale);
         $resultScale = self::determineScale($num);
 
         $scale = $resultScale > self::MAX_EXPANSION_SCALE
@@ -494,5 +494,35 @@ final readonly class Number implements Stringable
             $wholeNumber,
             $decimalNumber,
         ];
+    }
+
+    /**
+     * Certain mathematical equations are performed with a temp scale (20) to ensure no
+     * decimal points are lost. However this adds a huge trailing decimal string of many
+     * zeros. Because the precision of the equation's final output may be influenced by
+     * whether or not the computed number needs a high precision, we will remove any of
+     * the trailing decimal zeros down, to the minimum scale provided, to allow us to
+     * later determine the true precision/scale of the number.
+     *
+     * E.g.
+     *      "3.45"                  with scale 2             -> "3.45"
+     *      "3.4500000000000"       with scale 2             -> "3.45"
+     *      "3.4500000000000"       with scale 4             -> "3.4500"
+     *      "3.4543789543987"       with scale 4             -> "3.454378954398753453"
+     */
+    protected static function removeSuperfluousPrecision(string $num, int $minScale): string
+    {
+        [$wholeNumber, $decimalNumber] = self::parseFragments($num);
+
+        if ($decimalNumber === '') {
+            return $wholeNumber;
+        }
+
+        $decimalKeep = substr($decimalNumber, 0, $minScale);
+
+        $decimalTail = substr($decimalNumber, $minScale);
+        $decimalTail = rtrim($decimalTail, self::ZERO);
+
+        return $wholeNumber.self::DECIMAL_SEPARATOR.$decimalKeep.$decimalTail;
     }
 }
